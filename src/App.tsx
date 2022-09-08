88252657befbeb66a3631ffd5f43f9cc5ca66895
Marcos Petrungaro <marcos.petrungaro@gmail.com>
2022-09-08 10:22:40 -0300

    Adding some missing resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import styled from 'styled-components'
 
-function App() {
+const StyledH1 = styled.h1`
+  color: blue;
+`
+
+const teste = (texto: TemplateStringsArray) => {
+  console.log(texto)
+}
+
+teste`
+  a
+`
+
+console.log(styled.h1)
+
+// export interface ThemedStyledFunctionBase<
+//     C extends keyof JSX.IntrinsicElements | React.ComponentType<any>,
+//     T extends object,
+//     O extends object = {},
+//     A extends keyof any = never
+// > {
+//     (first: TemplateStringsArray): StyledComponent<C, T, O, A>;
+//     (
+//         first:
+//             | TemplateStringsArray
+//             | InterpolationFunction<ThemedStyledProps<StyledComponentPropsWithRef<C> & O, T>>,
+//         ...rest: Array<Interpolation<ThemedStyledProps<StyledComponentPropsWithRef<C> & O, T>>>
+//     ): StyledComponent<C, T, O, A>;
+//     <U extends object>(
+//         first:
+//             | TemplateStringsArray
+//             | CSSObject
+//             | InterpolationFunction<ThemedStyledProps<StyledComponentPropsWithRef<C> & O & U, T>>,
+//         ...rest: Array<Interpolation<ThemedStyledProps<StyledComponentPropsWithRef<C> & O & U, T>>>
+//     ): StyledComponent<C, T, O & U, A>;
+// }
+
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <StyledH1>Olá, mundo!</StyledH1>
       </header>
     </div>
-  );
+  )
 }
-
-export default App;
